e7d83e4519c737f31fc1f9e92c7cdd6195b7d3dc
Paul Frankovich <pmfrankovich@gmail.com>
2020-02-08 15:47:21 -0500

    Added delay timer

--- POC/OneOnOne.py
+++ POC/OneOnOne.py
@@ -3,8 +3,10 @@ from monsters import monster
 import combat
 from dice import roll
 from random import choice
+from time import sleep
 
 print(f'You are {player.name}, the {player.race} {player.playerclass}. You have encounted a {monster.name}.')
+sleep(1)
 
 suprises = choice(['player','monster',None])
 
@@ -12,6 +14,7 @@ suprises = choice(['player','monster',None])
 if suprises:
     target = 'player' if suprises == 'monster' else 'monster'
     print(f'{locals()[suprises].name} has a chance to suprise {locals()[target].name}.')
+    sleep(1)
     if suprises == 'monster':
         bonus = 0
     else:
@@ -22,8 +25,12 @@ else:
 
 if surpised:
     print(f'{locals()[target].name} has been surprised by {locals()[suprises].name}.')
+    sleep(1)
+    
     if suprises == 'player':
         print('With the element of suprise you get one free action. What will you do.')
+        sleep(1)
+        
         action = ''
         while action != 'A' and action != 'C':
             action = input('Action? (A)ttack (C)ast ')
@@ -35,24 +42,30 @@ if surpised:
         else:
             options = player.spellsknown['1']
         print('Select an option')
+        
         for option in options:
             print(option)
         selection = None
         while selection not in options:
             selection = input('?')
+            sleep(1)
         if action == 'A':
         		weapon = player.combat['Weapons'][selection]
         		if combat.attack(weapon['Bonus'], monster.combat['Armor Class']):
         				damage = roll(weapon['Damage'])
         				print(f'You have hit {monster.name} for {damage} points of damage.')
+        				sleep(1) 
         		else:
         				print('Miss')
     else:
         print(f'You have been surprised as {monster.name} attacks you.')
+        sleep(1)
+        
         weapon = monster.combat['actions']['morning star']
         if combat.attack(weapon['attack'], player.combat['Armor Class']):
             damage = weapon['hit']
             print(f'You where hit by {monster.name} for {damage} points of damange.')
+            sleep(1)
         else:
             print('Miss')
             
